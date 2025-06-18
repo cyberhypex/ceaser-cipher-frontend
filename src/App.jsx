900fa00bffe6d1417965f26474aea713b5f82db9
Anshuman <chaoanxuman999@gmail.com>
2025-06-18 23:59:54 +0530

    Favicon change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './output.css'
 import { LandingPage } from './components/LandingPage'
 import  NavBar  from './components/NavBar'
